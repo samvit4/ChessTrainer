f95f11f2f174403bfc327036372293243772b7d4
Samvit Dammalapati <samvit1998@gmail.com>
2026-01-05 23:43:25 +0530

    add unique client id

--- src/auth.ts
+++ src/auth.ts
@@ -5,7 +5,7 @@ import { BASE_PATH } from './routing';
 export const lichessHost = 'https://lichess.org';
 // export const lichessHost = 'http://l.org';
 export const scopes = ['board:play'];
-export const clientId = 'lichess-api-demo';
+export const clientId = 'sumwith-lichess-trainer';
 export const clientUrl = `${location.protocol}//${location.host}${BASE_PATH || '/'}`;
 
 export interface Me {
